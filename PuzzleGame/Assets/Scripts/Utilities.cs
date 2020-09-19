bfce3a6f80e7fe05edaa7730bfa9237b37c5c7fc
Tongwei Dai <tongweid@usc.edu>
2020-09-18 21:50:31 -0700

    project set up

---
+++ PuzzleGame/Assets/Scripts/Utilities.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PuzzleGame
+{
+    public static class ScreenSize
+    {
+        public static float ScreenToWorldHeight
+        {
+            get
+            {
+                Vector2 topRightCorner = new Vector2(1, 1);
+                Vector2 edgeVector = Camera.main.ViewportToWorldPoint(topRightCorner);
+                var height = edgeVector.y * 2;
+                return height;
+            }
+        }
+        public static float ScreenToWorldWidth
+        {
+            get
+            {
+                Vector2 topRightCorner = new Vector2(1, 1);
+                Vector2 edgeVector = Camera.main.ViewportToWorldPoint(topRightCorner);
+                var width = edgeVector.x * 2;
+                return width;
+            }
+        }
+    }
+}
